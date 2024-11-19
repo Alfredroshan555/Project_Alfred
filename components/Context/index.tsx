6adbc1bc654846b50789ec83927dc8327ec1cde6
Alfred <alfredroshan555@gmail.com>
2024-11-19 18:13:03 +0530

    Second commit: change made; testing git pushes

--- components/Context/index.tsx
+++ components/Context/index.tsx
@@ -1,14 +1,14 @@
 "use client";
-import useContextWrapper from "@/context/userContext";
+import useContextWrapper, { TContextType } from "@/context/userContext";
 import { Button } from "../ui/button";
 import "./styles.css";
 
 export const ContextHolderComponent = () => {
-  const { value, incrementValue } = useContextWrapper();
+  const { value, incrementValue }: TContextType = useContextWrapper(); // non-null assertion operator (!)
   return (
     <div className="context_wrapper">
       <h2>Learn React Context Here</h2>
-      <div>Context Value:: - {value}</div>
+      <div>Context Value:: {value}</div>
       <Button onClick={incrementValue}>Increment Value</Button>
     </div>
   );
