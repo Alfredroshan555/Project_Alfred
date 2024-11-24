919266685e809ec70a01697d3c73581a68512d36
Alfredroshan555 <alfredroshan555@gmail.com>
2024-11-25 00:06:56 +0530

    File upload completed

--- components/fileUploader/index.tsx
+++ components/fileUploader/index.tsx
@@ -2,37 +2,57 @@
 import { ChangeEvent, useState } from "react";
 import { Button } from "../ui/button";
 import axios from "axios";
+
+type UploadStatusType = "uploading" | "success" | "started" | "error";
 export const FileUploader = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [uploadStatus, setUploadStatus] = useState<UploadStatusType>();
+  const [progress, setProgress] = useState<number>(0);
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) =>
     e.target.files ? setFile(e.target.files[0]) : null;
-  };
 
   const handleFileUpload = async () => {
     if (!file) return;
 
+    setUploadStatus("uploading");
     const formData = new FormData();
     formData.append("file", file);
 
     try {
       await axios.post("https://httpbin.org/post", formData, {
         headers: { "Content-Type": "multipart/form-data" },
-        onUploadProgress: (progress) => {
-          console.log(progress);
+        onUploadProgress: (progressEvent) => {
+          console.log("Progress: ", progressEvent);
+          const progress = progressEvent.total
+            ? Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            : 0;
+          setProgress(progress);
         },
       });
-      console.log("uploaded");
+      setProgress(100);
+      setUploadStatus("success");
     } catch (error) {
-      console.log(error);
+      setUploadStatus("error");
+      setProgress(0);
     }
   };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      {file && <p>Selected file: {file.name}</p>}
-      <Button onClick={handleFileUpload}>Upload</Button>
+      {file && <p>Selected file: {file?.name}</p>}
+      {file && uploadStatus !== "uploading" && (
+        <Button onClick={handleFileUpload}>Upload</Button>
+      )}
+      {uploadStatus === "uploading" && <>Progress {progress}%</>}
+      {uploadStatus === "success" && (
+        <p className="text-sm text-green-600">File uploaded successfully!</p>
+      )}
+
+      {uploadStatus === "error" && (
+        <p className="text-sm text-red-600">Upload failed. Please try again.</p>
+      )}
     </div>
   );
 };
